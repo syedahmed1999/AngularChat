b79013275e7b150ccc07253c1fed85000b92862e
@ahmed.ali5 <ahmed.ali@tkxel.io>
2023-02-01 00:40:58 +0500

    adding components, modules, services etc

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
+
+@NgModule({
+  declarations: [InputComponent, ButtonComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
